9f18a7799f6f20b59d934d3f761ab2b64e09a20f
= <=>
2024-11-26 16:31:07 -0800

    componente header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './componentes/header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola mundo
         </p>
         <a
           className="App-link"
