e9579e06d8ee5bf12d51240a54656f037276f0ae
= <=>
2024-11-28 19:17:39 -0800

    componente lista y boton

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,13 +1,26 @@
 import "./Formulario.css"
 import CampoTexto from "../CampoTexto"
+import ListaOpciones from "../ListaOpciones"
+import Boton from "../Boton"
+
+
 
 function Formulario(){
+    function enviarForm(e){
+        e.preventDefault()
+        console.log("Form enviado")
+    }
+
     return <section className="secction__form">
-                <form>
+                <form onSubmit={enviarForm}>
                     <h2>Rellena el formulario para crear el colaborador.</h2>
                     <CampoTexto name="Nombre" placeholder="Ingresar nombre"/>
                     <CampoTexto name="Puesto" placeholder="Ingresar puesto"/>
                     <CampoTexto name="Foto" placeholder="Ingresar enlace de foto"/>
+                    <ListaOpciones />
+                    <Boton> 
+                        Crear
+                    </Boton>
                 </form>
             </section>
 }
